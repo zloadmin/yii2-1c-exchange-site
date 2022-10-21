8ba5c5c989d3390bcf3a974755c881f2345365fa
Vladimir Karpovich <dev.karpovich@gmail.com>
2022-10-21 11:55:59 +0400

    fix

--- web/index.php
+++ web/index.php
@@ -4,8 +4,8 @@
 //Режим дебага отключаем, т.к. при импорте, память копится от AR
 
 //if (isset($_SERVER['SERVER_ADDR']) && $_SERVER['SERVER_ADDR'] == '127.0.0.1' && strpos($_SERVER['REQUEST_URI'], '1c_exchange.php') === false) {
-    defined('YII_DEBUG') or define('YII_DEBUG', true);
-    defined('YII_ENV') or define('YII_ENV', 'dev');
+//    defined('YII_DEBUG') or define('YII_DEBUG', true);
+//    defined('YII_ENV') or define('YII_ENV', 'dev');
 //}
 
 //Yii::getLogger()->autoFlush = 1;
